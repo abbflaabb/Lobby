6cd6fc483c68a05aea1a2f06c6c42ea7143cbe43
abbflaabb <113551009+abbflaabb@users.noreply.github.com>
2025-03-20 20:51:44 +0300

    Create help.java

---
+++ src/main/java/com/abbas/lobby/commands/PlayerCommands/help.java
@@ -0,0 +1,32 @@
+package com.abbas.lobby.commands.PlayerCommand;
+
+import com.abbas.lobby.Utils.ColorUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class help implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String lable, String[] args) {
+        if (command.getName().equalsIgnoreCase("help")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("You must be a player to use this command!");
+                return true;
+            }
+            Player p = (Player) sender;
+            if (!p.hasPermission("lobby.help")) {
+                p.sendMessage("You do not have permission to use this command!");
+                return true;
+            }
+            p.sendMessage(ColorUtils.translateColorCodes("&6&l/lobby - &e&lMain command for the lobby"));
+            p.sendMessage(ColorUtils.translateColorCodes("&6&l/ping - Check your ping"));
+            p.sendMessage(ColorUtils.translateColorCodes("&6&l/support - Get support from a staff member"));
+            p.sendMessage(ColorUtils.translateColorCodes("&6&l/information - Get information about the server"));
+            return true;
+        }
+
+        return false;
+    }
+}
