e08aa81f3a8a383d36401090bfa2061bc9cc510a
abbflaabb <113551009+abbflaabb@users.noreply.github.com>
2025-03-20 20:50:09 +0300

    Update Lobby.java

--- src/main/java/com/abbas/lobby/Lobby.java
+++ src/main/java/com/abbas/lobby/Lobby.java
@@ -1,17 +1,46 @@
 package com.abbas.lobby;
 
+import com.abbas.lobby.Utils.Config;
+import com.abbas.lobby.commands.AdminCommand.*;
+import com.abbas.lobby.commands.PlayerCommand.*;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
 public final class Lobby extends JavaPlugin {
+    private final Logger logger = getLogger();
+
+    public ArrayList<Player> vanish_list = new ArrayList<>();
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        logger.info("Lobby plugin Enabled");
+        addcommands();
+        listeners();
+
+        Config.setup();
+        if (!Config.getConfig().contains("prefix")) {
+            Config.getConfig().set("prefix", "&8[&6Lobby&8]");
+            Config.save();
+        }
+    }
+
+    public void addcommands(){
+        getCommand("ping").setExecutor(new Ping());
+        getCommand("help").setExecutor(new help());
+        getCommand("kick").setExecutor(new Kick());
+        getCommand("support").setExecutor(new Support());
+    }
+
+    public void listeners(){
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        logger.info("Lobby plugin Disabled");
     }
+
 }
