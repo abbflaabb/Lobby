5c857970beee0f835fd9de81b7cadebe8335dd39
abbflaabb <113551009+abbflaabb@users.noreply.github.com>
2025-03-20 20:51:59 +0300

    Create Ping.java

---
+++ src/main/java/com/abbas/lobby/commands/PlayerCommands/Ping.java
@@ -0,0 +1,24 @@
+package com.abbas.lobby.commands.PlayerCommand;
+
+import com.abbas.lobby.Utils.ColorUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+
+public class Ping implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label,  String[] args) {
+        if (command.getName().equalsIgnoreCase("ping")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                int ping = ((CraftPlayer) player).getHandle().ping;
+
+                player.sendMessage(ColorUtils.translateColorCodes("Your ping is: " + ping + "ms"));
+            }
+        }
+        return true;
+    }
+}
