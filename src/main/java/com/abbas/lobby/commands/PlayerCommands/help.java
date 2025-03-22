94ef4cb6e8f255ffaeff5d76bbec35713b9e57b2
abbflaabb <113551009+abbflaabb@users.noreply.github.com>
2025-03-22 22:14:33 +0300

    V 1

--- src/main/java/com/abbas/lobby/commands/PlayerCommands/help.java
+++ src/main/java/com/abbas/lobby/commands/PlayerCommands/help.java
@@ -1,4 +1,4 @@
-package com.abbas.lobby.commands.PlayerCommand;
+package com.abbas.lobby.commands.PlayerCommands;
 
 import com.abbas.lobby.Utils.ColorUtils;
 import org.bukkit.command.Command;
@@ -12,18 +12,19 @@ public class help implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String lable, String[] args) {
         if (command.getName().equalsIgnoreCase("help")) {
             if (!(sender instanceof Player)) {
-                sender.sendMessage("You must be a player to use this command!");
+                sender.sendMessage("&cYou must be a player to use this command!");
                 return true;
             }
             Player p = (Player) sender;
             if (!p.hasPermission("lobby.help")) {
-                p.sendMessage("You do not have permission to use this command!");
+                p.sendMessage("&cYou do not have permission to use this command!");
                 return true;
             }
             p.sendMessage(ColorUtils.translateColorCodes("&6&l/lobby - &e&lMain command for the lobby"));
             p.sendMessage(ColorUtils.translateColorCodes("&6&l/ping - Check your ping"));
             p.sendMessage(ColorUtils.translateColorCodes("&6&l/support - Get support from a staff member"));
-            p.sendMessage(ColorUtils.translateColorCodes("&6&l/information - Get information about the server"));
+            p.sendMessage(ColorUtils.translateColorCodes("&6&l/information - Get information about the server - " + ColorUtils.translateColorCodes("&c&lComingSoon")));
+            p.sendMessage(ColorUtils.translateColorCodes("&6&l/menu - Show GamesMenu"));
             return true;
         }
 
