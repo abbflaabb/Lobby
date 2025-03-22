291a208677ff391f873ecf92bea0f5fb5596c5e9
abbflaabb <113551009+abbflaabb@users.noreply.github.com>
2025-03-22 22:08:01 +0300

    Update and rename Scoreboard.java to ScoreBoardListener.java

---
+++ src/main/java/com/abbas/lobby/Scoreobard/ScoreBoardListener.java
@@ -0,0 +1,26 @@
+package com.abbas.lobby.Scoreobard;
+
+import com.abbas.lobby.Lobby;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class ScoreBoardListener implements Listener {
+    private final ScoreBoardManager boardManager;
+
+    public ScoreBoardListener(Lobby plugin) {
+        this.boardManager = new ScoreBoardManager(plugin);
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        boardManager.setScoreboard(event.getPlayer());
+        boardManager.updateScoreboard();
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        boardManager.updateScoreboard();
+    }
+}
