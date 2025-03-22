ad8b5c7022a08900a9b697d885b32f2adcc67241
abbflaabb <113551009+abbflaabb@users.noreply.github.com>
2025-03-22 22:15:42 +0300

    V 1

--- src/main/java/com/abbas/lobby/commands/AdminCommands/Vanish.java
+++ src/main/java/com/abbas/lobby/commands/AdminCommands/Vanish.java
@@ -1,20 +1,16 @@
-package com.abbas.lobby.commands.AdminCommand;
+package com.abbas.lobby.commands.AdminCommands;
 
 import com.abbas.lobby.Lobby;
+import com.abbas.lobby.Utils.ColorUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class Vanish implements CommandExecutor {
-
-    Lobby plugin;
-
-    public Vanish(Lobby plugin) {
-        this.plugin = plugin;
-    }
-
+    private Lobby plugin;
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -24,10 +20,23 @@ public class Vanish implements CommandExecutor {
                 if(player.hasPermission("lobby.vanish")) {
                     if(plugin.vanish_list.contains(player)) {
                         for (Player people: Bukkit.getOnlinePlayers()) {
-                            people.showPlayer(plugin, player);
+                            people.showPlayer(player);
                         }
                         plugin.vanish_list.remove(player);
+                        player.sendMessage(ColorUtils.translateColorCodes("&cYou are no longer in vanish"));
+                    } else if (!plugin.vanish_list.contains(player)) {
+                        for (Player people : Bukkit.getOnlinePlayers()) {
+                            if (!people.hasPermission("lobby.vanish")) {
+                                people.hidePlayer(player);
+                            } else {
+                                people.showPlayer(player);
+                            }
+                        }
+                        plugin.vanish_list.add(player);
+                        player.sendMessage(ColorUtils.translateColorCodes("&aYou are now in vanish"));
                     }
+                } else {
+                    player.sendMessage(ColorUtils.translateColorCodes("&cYou do not have permission to run this command"));
                 }
             }
             return true;
