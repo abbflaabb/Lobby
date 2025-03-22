c62d1f27bbcdc04bea7550b6afb6400bbd477e1c
abbflaabb <113551009+abbflaabb@users.noreply.github.com>
2025-03-22 22:07:02 +0300

    V 1

--- src/main/java/com/abbas/lobby/Lobby.java
+++ src/main/java/com/abbas/lobby/Lobby.java
@@ -1,9 +1,15 @@
 package com.abbas.lobby;
 
-import com.abbas.lobby.Utils.Config;
-import com.abbas.lobby.commands.AdminCommand.*;
-import com.abbas.lobby.commands.PlayerCommand.*;
+import com.abbas.lobby.Listeners.PlayerBlockBreakEvent;
+import com.abbas.lobby.Listeners.joinListener;
+import com.abbas.lobby.Scoreobard.ScoreBoardListener;
+import com.abbas.lobby.commands.AdminCommands.*;
+import com.abbas.lobby.commands.PlayerCommands.*;
+import com.abbas.lobby.menus.MenuGamesListener;
+import com.abbas.lobby.menus.command.openMenuCommand;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
@@ -11,7 +17,7 @@ import java.util.logging.Logger;
 
 public final class Lobby extends JavaPlugin {
     private final Logger logger = getLogger();
-
+    private static Lobby instance;
     public ArrayList<Player> vanish_list = new ArrayList<>();
 
     @Override
@@ -19,12 +25,8 @@ public final class Lobby extends JavaPlugin {
         logger.info("Lobby plugin Enabled");
         addcommands();
         listeners();
+        instance = this;
 
-        Config.setup();
-        if (!Config.getConfig().contains("prefix")) {
-            Config.getConfig().set("prefix", "&8[&6Lobby&8]");
-            Config.save();
-        }
     }
 
     public void addcommands(){
@@ -32,15 +34,30 @@ public final class Lobby extends JavaPlugin {
         getCommand("help").setExecutor(new help());
         getCommand("kick").setExecutor(new Kick());
         getCommand("support").setExecutor(new Support());
+        getCommand("vanish").setExecutor(new Vanish());
+        getCommand("Discord").setExecutor(new Discord());
+        getCommand("menu").setExecutor(new openMenuCommand());
     }
 
     public void listeners(){
-
+        PluginManager p = Bukkit.getPluginManager();
+        p.registerEvents(new ScoreBoardListener(this), this);
+        p.registerEvents(new PlayerBlockBreakEvent(), this);
+        p.registerEvents(new MenuGamesListener(), this);
+        p.registerEvents(new joinListener(), this);
+        getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+        p.registerEvents(new MenuGamesListener(), this);
     }
 
     @Override
     public void onDisable() {
         logger.info("Lobby plugin Disabled");
+        instance = null;
+
+    }
+
+    public static Lobby getInstance() {
+        return instance;
     }
 
 }
