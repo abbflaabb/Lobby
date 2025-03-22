24d73a1d791824f4d92fa6d5d09c88768c85c73d
abbflaabb <113551009+abbflaabb@users.noreply.github.com>
2025-03-22 22:09:25 +0300

    V 1

---
+++ src/main/java/com/abbas/lobby/Listeners/joinListener.java
@@ -0,0 +1,29 @@
+package com.abbas.lobby.Listeners;
+
+import com.abbas.lobby.Utils.ColorUtils;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class joinListener implements Listener {
+
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        String JoinText = ColorUtils.translateColorCodes("Welcome " + player.getName() + "to the server!");
+        event.setJoinMessage(JoinText);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+
+        String quitText = ColorUtils.translateColorCodes(player.getName() + "has left the server!");
+        event.setQuitMessage(quitText);
+    }
+}
