0226f3bf9bc9ef591ff9d44ff5bf0b80d7497b98
abbflaabb <113551009+abbflaabb@users.noreply.github.com>
2025-03-28 00:28:38 +0300

    Update help.java

--- src/main/java/com/abbas/lobby/commands/PlayerCommands/help.java
+++ src/main/java/com/abbas/lobby/commands/PlayerCommands/help.java
@@ -1,25 +1,41 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.abbas.lobby.commands.PlayerCommands;
 
-import org.bukkit.ChatColor;
+import com.abbas.lobby.Utils.ColorUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+public class help implements CommandExecutor {
+    public help() {
+    }
 
-public class Discord implements CommandExecutor {
-    @Override
-    public boolean onCommand(CommandSender Sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("discord")) {
-            if (Sender instanceof Player) {
-                Player player = (Player) Sender;
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&3&l &9&lDiscord "));
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&3&l Join our discord server"));
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&3&l www.discord.com/example.com"));
+    public boolean onCommand(CommandSender sender, Command command, String lable, String[] args) {
+        if (command.getName().equalsIgnoreCase("help")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(ColorUtils.translateColorCodes("&cYou must be a player to use this command!"));
+                return true;
+            } else {
+                Player p = (Player)sender;
+                if (!p.hasPermission("lobby.help")) {
+                    p.sendMessage("&cYou do not have permission to use this command!");
+                    return true;
+                } else {
+                    p.sendMessage(ColorUtils.translateColorCodes("&6&l/lobby - &e&lMain command for the lobby"));
+                    p.sendMessage(ColorUtils.translateColorCodes("&6&l/ping - Check your ping"));
+                    p.sendMessage(ColorUtils.translateColorCodes("&6&l/support - Get support from a staff member"));
+                    p.sendMessage(ColorUtils.translateColorCodes("&6&l/information - Get information about the server - " + ColorUtils.translateColorCodes("&c&lComingSoon")));
+                    p.sendMessage(ColorUtils.translateColorCodes("&6&l/menu - Show GamesMenu"));
+                    return true;
+                }
             }
-            return true;
+        } else {
+            return false;
         }
-
-        return false;
     }
 }
