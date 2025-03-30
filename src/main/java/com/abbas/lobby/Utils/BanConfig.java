64b6093dff0abac34fe382dd825b5ccf9ebd4466
Abbas <113551009+abbflaabb@users.noreply.github.com>
2025-03-30 22:24:30 +0300

    Update BanConfig.java

--- src/main/java/com/abbas/lobby/Utils/BanConfig.java
+++ src/main/java/com/abbas/lobby/Utils/BanConfig.java
@@ -8,14 +8,15 @@ public class BanConfig {
         Config.setup();
         FileConfiguration config = Config.getConfig();
         if (!config.isConfigurationSection("banMessages")) {
-            config.set("banMessages.ban.success", "&aSuccessfully banned %player% for %reason%");
+            config.set("banMessages.ban.success", "&aSuccessfully banned %player% for %reason% (Ban ID: %ban_id%)");
             config.set("banMessages.ban.noPermission", "&cYou do not have permission to ban!");
             config.set("banMessages.ban.usage", "&cUsage: /ban <player> [duration] <reason>");
             config.set("banMessages.ban.playerNotFound", "&cPlayer not found.");
             config.set("banMessages.ban.kickMessage", "§c§lLobbyBan§7» §cBanned for %reason%\n"
                     + "§4§oBanned by CONSOLE\n\n"
                     + "§7It seems like you are using a hacked client, please disable it!\n"
-                    + "§cUnban in §f> §e%timeLeft%\n\n"
+                    + "§cUnban in §f> §e%timeLeft%\n"
+                    + "§7Ban ID: §e%ban_id%\n\n"
                     + "§7Unban application in TS or forum\n"
                     + "§eTS-Ip§7 » §ccoming soon\n"
                     + "§eForum§7 » §ccoming soon");
@@ -24,11 +25,13 @@ public class BanConfig {
             config.set("banMessages.ban.advancedBanMessage", "§c§lLobbyBan §7» §cBanned for %reason%\n"
                     + "§4§oBanned by CONSOLE\n\n"
                     + "§7It seems like you are using a hacked client, please disable it!\n"
-                    + "§cUnban in §f> §e%timeLeft%\n\n"
+                    + "§cUnban in §f> §e%timeLeft%\n"
+                    + "§7Ban ID: §e%ban_id%\n\n"
                     + "§7Unban application in TS or forum\n"
                     + "§eTS-Ip§7 » §ccoming soon\n"
                     + "§eForum§7 » §ccoming soon");
             config.set("banMessages.ban.forumLink", "§eForum§7 » §ccoming soon");
+
             Config.save();
         }
     }
