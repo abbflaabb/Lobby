19d48b055b2df41e9a935cee62203a825a55d8e1
Abbas <113551009+abbflaabb@users.noreply.github.com>
2025-04-10 22:15:05 +0300

    Update BanConfig.java

--- src/main/java/com/abbas/lobby/Utils/BanConfig.java
+++ src/main/java/com/abbas/lobby/Utils/BanConfig.java
@@ -8,11 +8,11 @@ public class BanConfig {
         Config.setup();
         FileConfiguration config = Config.getConfig();
         if (!config.isConfigurationSection("banMessages")) {
-            config.set("banMessages.ban.success", "&aSuccessfully banned %player% for %reason% (Ban ID: %ban_id%)");
-            config.set("banMessages.ban.noPermission", "&cYou do not have permission to ban!");
-            config.set("banMessages.ban.usage", "&cUsage: /ban <player> [duration] <reason>");
-            config.set("banMessages.ban.playerNotFound", "&cPlayer not found.");
-            config.set("banMessages.ban.kickMessage", "§c§lLobbyBan§7» §cBanned for %reason%\n"
+            config.set("banMessages.success", "&aSuccessfully banned %player% for %reason% (Ban ID: %ban_id%)");
+            config.set("banMessages.noPermission", "&cYou do not have permission to ban!");
+            config.set("banMessages.usage", "&cUsage: /ban <player> [duration] <reason>");
+            config.set("banMessages.playerNotFound", "&cPlayer not found.");
+            config.set("banMessages.kickMessage", "§c§lLobbyBan§7» §cBanned for %reason%\n"
                     + "§4§oBanned by CONSOLE\n\n"
                     + "§7It seems like you are using a hacked client, please disable it!\n"
                     + "§cUnban in §f> §e%timeLeft%\n"
@@ -20,9 +20,9 @@ public class BanConfig {
                     + "§7Unban application in TS or forum\n"
                     + "§eTS-Ip§7 » §ccoming soon\n"
                     + "§eForum§7 » §ccoming soon");
-            config.set("banMessages.ban.invalidDuration", "&cInvalid duration format. Use s (seconds), m (minutes), h (hours), or d (days).");
-            config.set("banMessages.ban.expiration", "&aBan expires on %date%");
-            config.set("banMessages.ban.advancedBanMessage", "§c§lLobbyBan §7» §cBanned for %reason%\n"
+            config.set("banMessages.invalidDuration", "&cInvalid duration format. Use s (seconds), m (minutes), h (hours), or d (days).");
+            config.set("banMessages.expiration", "&aBan expires on %date%");
+            config.set("banMessages.advancedBanMessage", "§c§lLobbyBan §7» §cBanned for %reason%\n"
                     + "§4§oBanned by CONSOLE\n\n"
                     + "§7It seems like you are using a hacked client, please disable it!\n"
                     + "§cUnban in §f> §e%timeLeft%\n"
@@ -30,7 +30,7 @@ public class BanConfig {
                     + "§7Unban application in TS or forum\n"
                     + "§eTS-Ip§7 » §ccoming soon\n"
                     + "§eForum§7 » §ccoming soon");
-            config.set("banMessages.ban.forumLink", "§eForum§7 » §ccoming soon");
+            config.set("banMessages.forumLink", "§eForum§7 » §ccoming soon");
 
             Config.save();
         }
