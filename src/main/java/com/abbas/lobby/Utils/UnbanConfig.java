d10d5ff06f27d38226f4aeb2f617d1284f91ce64
Abbas <113551009+abbflaabb@users.noreply.github.com>
2025-04-23 22:07:01 +0300

    Leave Beta 1.4.2

--- src/main/java/com/abbas/lobby/Utils/UnbanConfig.java
+++ src/main/java/com/abbas/lobby/Utils/UnbanConfig.java
@@ -8,10 +8,10 @@ public class UnbanConfig {
         Config.setup();
         FileConfiguration config = Config.getConfig();
         if (!config.isConfigurationSection("unbanMessages")) {
-            config.set("unbanMessages.success", "&aSuccessfully unbanned %player%");
-            config.set("unbanMessages.noPermission", "&cYou do not have permission to unban!");
-            config.set("unbanMessages.usage", "&cUsage: /unban <player>");
-            config.set("unbanMessages.notBanned", "&cThat player is not banned.");
+            config.set("unbanMessages.success", "&a✔ Successfully unbanned %player%");
+            config.set("unbanMessages.noPermission", "&c⚠ You do not have permission to unban players!");
+            config.set("unbanMessages.usage", "&c⚠ Usage: /unban <player>");
+            config.set("unbanMessages.notBanned", "&c⚠ That player is not banned!");
             Config.save();
         }
     }
