f9403f1ef307a0aa0d3e8c92061ef413b1e0ed0d
Abbas <113551009+abbflaabb@users.noreply.github.com>
2025-04-23 22:07:15 +0300

    Update WarnConfig.java

--- src/main/java/com/abbas/lobby/Utils/WarnConfig.java
+++ src/main/java/com/abbas/lobby/Utils/WarnConfig.java
@@ -5,15 +5,15 @@ public class WarnConfig {
         Config.setup();
         FileConfiguration config = Config.getConfig();
         if (!config.isConfigurationSection("warnMessages")) {
-            config.set("warnMessages.warn.success", "&aSuccessfully warned %player% for %reason%");
-            config.set("warnMessages.warn.noPermission", "&cYou do not have permission to warn!");
-            config.set("warnMessages.warn.usage", "&cUsage: /warn <player> <reason>");
-            config.set("warnMessages.warn.playerNotFound", "&cPlayer %player% is not online!");
-            config.set("warnMessages.warn.exempt", "&cThis player cannot be warned!");
-            config.set("warnMessages.warn.header", "&c━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
+            config.set("warnMessages.warn.noPermission", "&c⚠ You do not have permission to warn players!");
+            config.set("warnMessages.warn.usage", "&c⚠ Usage: /warn <player> <reason>");
+            config.set("warnMessages.warn.playerNotFound", "&c⚠ Player %player% not found!");
+            config.set("warnMessages.warn.exempt", "&c⚠ You cannot warn this player!");
+            config.set("warnMessages.warn.header", "&7&m---------------------");
             config.set("warnMessages.warn.warningMessage", "&c⚠ You have been warned by &e%sender%");
-            config.set("warnMessages.warn.reasonMessage", "&cReason: &e%reason%");
-            config.set("warnMessages.warn.staffNotification", "&7[Staff] &e%player% &7was warned by &e%sender% &7for: &e%reason%");
+            config.set("warnMessages.warn.reasonMessage", "&7Reason: &f%reason%");
+            config.set("warnMessages.warn.success", "&a✔ Successfully warned %player% for: %reason%");
+            config.set("warnMessages.warn.staffNotification", "&7[Staff] &e%player% &7was warned by &e%sender% &7for: &f%reason%");
             Config.save();
         }
     }
