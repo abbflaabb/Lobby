3cc30cc3e6840a59e678324025735fecd190401d
Abbas <113551009+abbflaabb@users.noreply.github.com>
2025-04-23 22:06:36 +0300

    Leave Beta 1.4.2

--- src/main/java/com/abbas/lobby/Utils/MuteConfig.java
+++ src/main/java/com/abbas/lobby/Utils/MuteConfig.java
@@ -8,27 +8,31 @@ public class MuteConfig {
         FileConfiguration config = Config.getConfig();
 
         if (!config.isConfigurationSection("muteMessages")) {
-            config.set("muteMessages.success", "&aSuccessfully muted &e%player% &afor &e%duration%. Mute ID: &e%mute_id%");
-            config.set("muteMessages.successPermanent", "&aSuccessfully muted &e%player% &apermanently. Mute ID: &e%mute_id%");
-            config.set("muteMessages.noPermission", "&cYou don't have permission to use this command!");
-            config.set("muteMessages.usage", "&cUsage: /mute <player> [duration] <reason>");
-            config.set("muteMessages.playerNotFound", "&cPlayer not found!");
-            config.set("muteMessages.exempt", "&cThis player cannot be muted!");
-            config.set("muteMessages.alreadyMuted", "&cThis player is already muted!");
-            config.set("muteMessages.invalidDuration", "&cInvalid duration format! Use: <number>[s/m/h/d]");
+            config.set("muteMessages.noPermission", "&c⚠ You do not have permission to mute players!");
+            config.set("muteMessages.usage", "&c⚠ Usage: /mute <player> [duration] [reason]");
+            config.set("muteMessages.playerNotFound", "&c⚠ Player not found!");
+            config.set("muteMessages.exempt", "&c⚠ This player cannot be muted!");
+            config.set("muteMessages.alreadyMuted", "&c⚠ This player is already muted!");
+            config.set("muteMessages.invalidDuration", "&c⚠ Invalid duration format! Use s/m/h/d");
+            config.set("muteMessages.playerMuted", "&c⚠ You have been muted for %duration%\n&7Reason: &f%reason%\n&7Mute ID: &f%mute_id%");
+            config.set("muteMessages.playerMutedPermanent", "&c⚠ You have been permanently muted!\n&7Reason: &f%reason%\n&7Mute ID: &f%mute_id%");
+            config.set("muteMessages.success", "&a✔ Successfully muted %player% for %duration% (ID: %mute_id%)");
+            config.set("muteMessages.successPermanent", "&a✔ Successfully muted %player% permanently (ID: %mute_id%)");
 
-            config.set("muteMessages.playerMuted", "&cYou have been muted for &e%duration%&c. Reason: &e%reason%. Mute ID: &e%mute_id%");
-            config.set("muteMessages.playerMutedPermanent", "&cYou have been permanently muted. Reason: &e%reason%. Mute ID: &e%mute_id%");
 
             config.set("muteMessages.cannotChat", "&cYou cannot chat while muted! Time remaining: &e%timeLeft%");
             config.set("muteMessages.cannotChatPermanent", "&cYou cannot chat while permanently muted!");
 
-            config.set("muteMessages.unmute.success", "&aSuccessfully unmuted &e%player%");
-            config.set("muteMessages.unmute.notMuted", "&cThis player is not muted!");
-            config.set("muteMessages.unmute.noPermission", "&cYou don't have permission to unmute players!");
+
+            config.set("muteMessages.unmute.noPermission", "&c⚠ You do not have permission to unmute players!");
+            config.set("muteMessages.unmute.usage", "&c⚠ Usage: /unmute <player>");
+            config.set("muteMessages.unmute.notMuted", "&c⚠ That player is not muted!");
+            config.set("muteMessages.unmute.notFound", "&c⚠ Player not found or is offline!");
+            config.set("muteMessages.unmute.success", "&a✔ Successfully unmuted %player%");
+            config.set("muteMessages.unmute.target", "&a✔ You have been unmuted!");
+            config.set("muteMessages.staff.unmuteNotification", "&7[Staff] &e%player% &7was unmuted by &e%unmuter%");
 
             config.set("muteMessages.staff.notification", "&7[Staff] &e%player% &7was muted by &e%muter% &7for: &e%reason%. Mute ID: &e%mute_id%");
-            config.set("muteMessages.staff.unmuteNotification", "&7[Staff] &e%player% &7was unmuted by &e%unmuter%");
 
             Config.save();
         }
