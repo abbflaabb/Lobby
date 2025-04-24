bd3aeef422cd697083b92cef4914ff71c9dfb0b5
Abbas <Abbas@gamil.com>
2025-04-24 17:30:25 +0300

    API USED

--- src/main/java/com/abbas/lobby/Listeners/BlockPlace.java
+++ src/main/java/com/abbas/lobby/Listeners/BlockPlace.java
@@ -1,7 +1,5 @@
 package com.abbas.lobby.Listeners;
 
-import com.abbas.lobby.API.ConfigAPI.ConfigPath;
-import com.abbas.lobby.API.EventsAPI.BlockPlaceAPI;
 import com.abbas.lobby.Utils.ColorUtils;
 import com.abbas.lobby.Utils.Config;
 import org.bukkit.Effect;
@@ -12,48 +10,43 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 
-public class BlockPlace implements Listener, BlockPlaceAPI {
 
+public class BlockPlace implements Listener {
     public BlockPlace() {
         setupConfig();
     }
 
-    @Override
-    public void setupConfig() {
+    private void setupConfig() {
         Config.setup();
         FileConfiguration config = Config.getConfig();
 
         if (!config.isConfigurationSection("blockPlace.messages")) {
-            config.set(ConfigPath.BLOCK_PLACE_NO_PERMISSION, "&c⚠ &7You cannot place blocks here!");
+            config.set("blockPlace.messages.noPermission", "&c⚠ &7You cannot place blocks here!");
             Config.save();
         }
     }
-
     @EventHandler
-    @Override
-    public void handleBlockPlace(BlockPlaceEvent event) {
+    public void onPlace(BlockPlaceEvent event) {
         event.setCancelled(true);
-        Player player = event.getPlayer();
+        Player p = event.getPlayer();
 
-        if (player.hasPermission("lobby.BlockPlace")) {
+        if (p.hasPermission("lobby.BlockPlace")) {
             event.setCancelled(false);
-            playSuccessEffects(player, event);
+            p.playSound(p.getLocation(), Sound.DIG_STONE, 1.0f, 1.0f);
+            p.playEffect(event.getBlock().getLocation().add(0.5, 0.5, 0.5), Effect.HAPPY_VILLAGER, 0);
             return;
         }
 
-        playDenyEffects(player, event);
+
+        p.sendMessage(ColorUtils.translateColorCodes(Config.getConfig().getString("blockPlace.messages.noPermission")));
+        p.playSound(p.getLocation(), Sound.VILLAGER_NO, 1.0f, 1.0f);
+        p.playEffect(event.getBlock().getLocation().add(0.5, 0.5, 0.5), Effect.SMOKE, 0);
     }
+}
+
 
     @Override
     public void playSuccessEffects(Player player, BlockPlaceEvent event) {
         player.playSound(player.getLocation(), Sound.DIG_STONE, 1.0f, 1.0f);
         player.playEffect(event.getBlock().getLocation().add(0.5, 0.5, 0.5), Effect.HAPPY_VILLAGER, 0);
     }
-
-    @Override
-    public void playDenyEffects(Player player, BlockPlaceEvent event) {
-        player.sendMessage(ColorUtils.translateColorCodes(Config.getConfig().getString(ConfigPath.BLOCK_PLACE_NO_PERMISSION)));
-        player.playSound(player.getLocation(), Sound.VILLAGER_NO, 1.0f, 1.0f);
-        player.playEffect(event.getBlock().getLocation().add(0.5, 0.5, 0.5), Effect.SMOKE, 0);
-    }
-}
