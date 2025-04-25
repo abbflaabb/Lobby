0f668de4f6d61de4809804c9bab8853f6fddb06e
Abbas <113551009+abbflaabb@users.noreply.github.com>
2025-04-25 15:09:42 +0300

    1.4.3 SnapShot

---
+++ src/main/java/com/abbas/lobby/API/ConfigAPI/ConfigCommandPath.java
@@ -0,0 +1,140 @@
+package com.abbas.lobby.API.ConfigAPI;
+
+public class ConfigCommandPath {
+    // Discord command paths
+    public static final String DISCORD_MESSAGE_1 = "discord.message-1";
+    public static final String DISCORD_MESSAGE_2 = "discord.message-2";
+    public static final String DISCORD_MESSAGE_3 = "discord.message-3";
+    public static final String DISCORD_NO_PERMISSION = "discord.noPermission";
+    public static final String DISCORD_PLAYER_ONLY = "discord.playerOnly";
+
+
+    public static final String FLY_NO_PERMISSION = "fly.noPermission";
+    public static final String FLY_PLAYER_ONLY = "fly.playerOnly";
+    public static final String FLY_ENABLED = "fly.enabled";
+    public static final String FLY_DISABLED = "fly.disabled";
+    public static final String FLY_HIGH_ENABLED = "fly.highEnabled";
+    public static final String FLY_HIGH_DISABLED = "fly.highDisabled";
+    public static final String FLY_NORMAL_SPEED = "fly.normalSpeed";
+    public static final String FLY_HIGH_SPEED = "fly.highSpeed";
+
+
+    public static final String LOBBY_NO_PERMISSION = "lobby.noPermission";
+    public static final String LOBBY_PLAYER_ONLY = "lobby.playerOnly";
+    public static final String LOBBY_COOLDOWN = "lobby.cooldown";
+    public static final String LOBBY_HEADER = "lobby.header";
+    public static final String LOBBY_FOOTER = "lobby.footer";
+    public static final String LOBBY_GENERAL_COMMANDS = "lobby.generalCommands";
+    public static final String LOBBY_PREMIUM_COMMANDS = "lobby.premiumCommands";
+    public static final String LOBBY_ADMIN_COMMANDS = "lobby.adminCommands";
+    public static final String LOBBY_COMMAND_LIST = "lobby.commandList";
+
+
+    public static final String INFO_NO_PERMISSION = "information.noPermission";
+    public static final String INFO_PLAYER_ONLY = "information.playerOnly";
+    public static final String INFO_RULES = "information.rules";
+    public static final String INFO_SOCIAL_DISCORD = "information.social.discord";
+    public static final String INFO_SOCIAL_WEBSITE = "information.social.website";
+    public static final String INFO_SOCIAL_STORE = "information.social.store";
+    public static final String INFO_SERVER_VERSION = "information.server.version";
+    public static final String INFO_SERVER_GAMEMODE = "information.server.gamemode";
+    public static final String INFO_STAFF_CONTACT = "information.staffContact";
+    public static final String INFO_QUICK_COMMANDS = "information.quickCommands";
+
+
+    public static final String PING_NO_PERMISSION = "ping.noPermission";
+    public static final String PING_PLAYER_ONLY = "ping.playerOnly";
+    public static final String PING_SELF_PING = "ping.selfPing";
+    public static final String PING_QUALITY_EXCELLENT = "ping.quality.excellent";
+    public static final String PING_QUALITY_GOOD = "ping.quality.good";
+    public static final String PING_QUALITY_MODERATE = "ping.quality.moderate";
+    public static final String PING_QUALITY_POOR = "ping.quality.poor";
+    public static final String PING_QUALITY_BAD = "ping.quality.bad";
+
+
+
+    public static final String SPAWN_NO_PERMISSION = "spawn.noPermission";
+    public static final String SPAWN_PLAYER_ONLY = "spawn.playerOnly";
+    public static final String SPAWN_TELEPORT = "spawn.teleport";
+    public static final String SPAWN_NOT_SET = "spawn.notSet";
+    public static final String SPAWN_SET_SUCCESS = "spawn.setspawn.success";
+    public static final String SPAWN_LOCATION = "spawnLocation";
+
+
+    public static final String SUPPORT_NO_PERMISSION = "support.noPermission";
+    public static final String SUPPORT_PLAYER_ONLY = "support.playerOnly";
+    public static final String SUPPORT_TITLE = "support.title";
+    public static final String SUPPORT_DISCORD = "support.discord";
+    public static final String SUPPORT_TEAMSPEAK = "support.teamspeak";
+    public static final String SUPPORT_FORUMS = "support.forums";
+
+
+    public static final String ADMIN_RELOAD_NO_PERMISSION = "adminMessages.reload.noPermission";
+    public static final String ADMIN_RELOAD_SUCCESS = "adminMessages.reload.success";
+    public static final String ADMIN_RELOAD_ERROR = "adminMessages.reload.error";
+
+    public static final String BAN_NO_PERMISSION = "banMessages.noPermission";
+    public static final String BAN_USAGE = "banMessages.usage";
+    public static final String BAN_PLAYER_NOT_FOUND = "banMessages.playerNotFound";
+    public static final String BAN_SUCCESS = "banMessages.success";
+    public static final String BAN_FORUM_LINK = "banMessages.forumLink";
+    public static final String BAN_PLAYER_ONLY = "banMessages.playerOnly";
+    public static final String BAN_IDS = "banIds.";
+    public static final String BAN_INVALID_DURATION = "banMessages.invalidDuration";
+    public static final String BAN_EXPIRATION = "banMessages.expiration";
+
+
+    public static final String KICK_NO_PERMISSION = "kickMessages.noPermission";
+    public static final String KICK_USAGE = "kickMessages.usage";
+    public static final String KICK_PLAYER_NOT_FOUND = "kickMessages.playerNotFound";
+    public static final String KICK_SUCCESS = "kickMessages.success";
+    public static final String KICK_DEFAULT_REASON = "kickMessages.defaultReason";
+    public static final String KICK_FORMAT = "kickMessages.kickFormat";
+    public static final String KICK_PLAYER_ONLY = "kickMessages.playerOnly";
+
+
+    public static final String MUTE_NO_PERMISSION = "muteMessages.noPermission";
+    public static final String MUTE_USAGE = "muteMessages.usage";
+    public static final String MUTE_PLAYER_NOT_FOUND = "muteMessages.playerNotFound";
+    public static final String MUTE_EXEMPT = "muteMessages.exempt";
+    public static final String MUTE_ALREADY_MUTED = "muteMessages.alreadyMuted";
+    public static final String MUTE_INVALID_DURATION = "muteMessages.invalidDuration";
+    public static final String MUTE_PLAYER_MUTED = "muteMessages.playerMuted";
+    public static final String MUTE_PLAYER_MUTED_PERMANENT = "muteMessages.playerMutedPermanent";
+    public static final String MUTE_SUCCESS = "muteMessages.success";
+    public static final String MUTE_SUCCESS_PERMANENT = "muteMessages.successPermanent";
+    public static final String MUTE_STAFF_NOTIFICATION = "muteMessages.staff.notification";
+
+
+    public static final String UNBAN_NO_PERMISSION = "unbanMessages.noPermission";
+    public static final String UNBAN_USAGE = "unbanMessages.usage";
+    public static final String UNBAN_NOT_BANNED = "unbanMessages.notBanned";
+    public static final String UNBAN_SUCCESS = "unbanMessages.success";
+
+
+    public static final String UNMUTE_NO_PERMISSION = "muteMessages.unmute.noPermission";
+    public static final String UNMUTE_USAGE = "muteMessages.unmute.usage";
+    public static final String UNMUTE_NOT_MUTED = "muteMessages.unmute.notMuted";
+    public static final String UNMUTE_NOT_FOUND = "muteMessages.unmute.notFound";
+    public static final String UNMUTE_SUCCESS = "muteMessages.unmute.success";
+    public static final String UNMUTE_TARGET = "muteMessages.unmute.target";
+    public static final String UNMUTE_STAFF_NOTIFICATION = "muteMessages.staff.unmuteNotification";
+
+
+    public static final String VANISH_NO_PERMISSION = "vanishMessages.noPermission";
+    public static final String VANISH_ENABLED = "vanishMessages.enabled";
+    public static final String VANISH_DISABLED = "vanishMessages.disabled";
+    public static final String VANISH_PLAYER_ONLY = "vanishMessages.playerOnly";
+
+    public static final String WARN_NO_PERMISSION = "warnMessages.warn.noPermission";
+    public static final String WARN_USAGE = "warnMessages.warn.usage";
+    public static final String WARN_PLAYER_NOT_FOUND = "warnMessages.warn.playerNotFound";
+    public static final String WARN_EXEMPT = "warnMessages.warn.exempt";
+    public static final String WARN_HEADER = "warnMessages.warn.header";
+    public static final String WARN_WARNING_MESSAGE = "warnMessages.warn.warningMessage";
+    public static final String WARN_REASON_MESSAGE = "warnMessages.warn.reasonMessage";
+    public static final String WARN_SUCCESS = "warnMessages.warn.success";
+    public static final String WARN_STAFF_NOTIFICATION = "warnMessages.warn.staffNotification";
+}
+
+
---
+++ src/main/java/com/abbas/lobby/API/ConfigAPI/ConfigPath.java
@@ -0,0 +1,46 @@
+package com.abbas.lobby.API.ConfigAPI;
+
+public class ConfigPath {
+
+    public static final String DROP_NO_PERMISSION = "drop.messages.noPermission";
+    public static final String BLOCK_BREAK_NO_PERMISSION = "blockBreak.messages.noPermission";
+    public static final String BLOCK_PLACE_NO_PERMISSION = "blockPlace.messages.noPermission";
+
+
+    public static final String SPAWN_TELEPORT = "messages.spawn.teleport";
+    public static final String SPAWN_NOT_SET = "messages.spawn.notSet";
+    public static final String SETSPAWN_SUCCESS = "messages.setspawn.success";
+    public static final String RESPAWN_TELEPORT = "messages.respawn.teleport";
+    public static final String RESPAWN_NOT_SET = "messages.respawn.notSet";
+    public static final String SPAWN_LOCATION = "spawnLocation";
+
+
+    public static final String JOIN_PREFIX = "join.prefix";
+    public static final String JOIN_MESSAGE_WELCOME = "join.messages.welcome";
+    public static final String JOIN_MESSAGE_LEAVE = "join.messages.leave";
+
+    public static final String TELEPORT_BOW_NAME = "teleport-bow.name";
+    public static final String TELEPORT_BOW_LORE = "teleport-bow.lore";
+    public static final String TELEPORT_BOW_ARROW_SPEED = "teleport-bow.arrow-speed";
+    public static final String TELEPORT_BOW_ENABLED = "teleport-bow.enabled";
+
+
+    public static final String TITLES_WELCOME_TITLE = "titles.welcome.title";
+    public static final String TITLES_WELCOME_SUBTITLE = "titles.welcome.subtitle";
+    public static final String TITLES_WELCOME_FADEIN = "titles.welcome.fadeIn";
+    public static final String TITLES_WELCOME_STAY = "titles.welcome.stay";
+    public static final String TITLES_WELCOME_FADEOUT = "titles.welcome.fadeOut";
+
+
+    // Scoreboard paths
+    public static final String SCOREBOARD_TITLE = "scoreboard.title";
+    public static final String SCOREBOARD_NO_PERMISSION = "scoreboard.No-permission";
+    public static final String SCOREBOARD_ENABLED_MESSAGE = "scoreboard.enabled-message";
+    public static final String SCOREBOARD_DISABLED_MESSAGE = "scoreboard.disabled-message";
+    public static final String SCOREBOARD_ERROR_MESSAGE = "scoreboard.error-message";
+    public static final String SCOREBOARD_PLAYER_ONLY = "scoreboard.player-only";
+    public static final String SCOREBOARD_LINES = "scoreboard.lines";
+    public static final String SCOREBOARD_UPDATE_INTERVAL = "scoreboard.update-interval";
+
+
+}
