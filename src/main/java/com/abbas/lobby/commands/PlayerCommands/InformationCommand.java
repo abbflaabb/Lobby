77e0a09548e55c2fc5e4b52f3a6a5603760be463
Abbas <113551009+abbflaabb@users.noreply.github.com>
2025-04-25 14:36:39 +0300

    1.4.3 SnapShot

--- src/main/java/com/abbas/lobby/commands/PlayerCommands/InformationCommand.java
+++ src/main/java/com/abbas/lobby/commands/PlayerCommands/InformationCommand.java
@@ -1,8 +1,9 @@
 package com.abbas.lobby.commands.PlayerCommands;
 
+import com.abbas.lobby.API.ConfigAPI.ConfigCommandPath;
+import com.abbas.lobby.API.MainAPIS.ICommandAPI;
 import com.abbas.lobby.Utils.ColorUtils;
 import com.abbas.lobby.Utils.Config;
-import com.abbas.lobby.API.ICommandAPI;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -24,35 +25,70 @@ public class InformationCommand implements ICommandAPI {
         FileConfiguration config = Config.getConfig();
 
         if (!config.isConfigurationSection("information")) {
-            config.set("information.noPermission", "&c⚠ You do not have permission to use this command!");
-            config.set("information.playerOnly", "&c⚠ Console cannot use this command!");
+            config.set(ConfigCommandPath.INFO_NO_PERMISSION, "&c⚠ You do not have permission to use this command!");
+            config.set(ConfigCommandPath.INFO_PLAYER_ONLY, "&c⚠ Console cannot use this command!");
 
-            config.set("information.rules", Arrays.asList(
+            config.set(ConfigCommandPath.INFO_RULES, Arrays.asList(
                     "&7• No hacking or exploiting.",
                     "&7• Be respectful to all players.",
                     "&7• No advertising or spamming."
             ));
 
-            config.set("information.social.discord", "https://discord.com/example");
-            config.set("information.social.website", "https://example.com");
-            config.set("information.social.store", "https://store.example.com");
+            config.set(ConfigCommandPath.INFO_SOCIAL_DISCORD, "https://discord.com/example");
+            config.set(ConfigCommandPath.INFO_SOCIAL_WEBSITE, "https://example.com");
+            config.set(ConfigCommandPath.INFO_SOCIAL_STORE, "https://store.example.com");
 
-            config.set("information.server.version", "1.20.1");
-            config.set("information.server.gamemode", "Survival");
+            config.set(ConfigCommandPath.INFO_SERVER_VERSION, "1.20.1");
+            config.set(ConfigCommandPath.INFO_SERVER_GAMEMODE, "Survival");
 
-            config.set("information.staffContact", Arrays.asList(
+            config.set(ConfigCommandPath.INFO_STAFF_CONTACT, Arrays.asList(
                     "&7• Contact admins via /discord.",
                     "&7• Report issues in our Discord server.",
                     "&7• For urgent matters, email support@example.com."
             ));
 
-            config.set("information.quickCommands", Arrays.asList(
+            config.set(ConfigCommandPath.INFO_QUICK_COMMANDS, Arrays.asList(
                     "&7• &b/spawn - &fReturn to spawn",
                     "&7• &b/help - &fView help commands"
             ));
 
             Config.save();
         }
+    }
+
+    private void sendServerInformation(CommandSender sender, FileConfiguration config) {
+        sender.sendMessage("");
+        sender.sendMessage(ChatColor.GRAY + "━━━━━━━━━━ " + ChatColor.RED + "Server Information" + ChatColor.GRAY + " ━━━━━━━━━━");
+        sender.sendMessage("");
+
+        sender.sendMessage(ChatColor.RED + "➤ " + ChatColor.YELLOW + "Server Rules:");
+        config.getStringList(ConfigCommandPath.INFO_RULES).forEach(rule ->
+                sender.sendMessage(ColorUtils.translateColorCodes(rule)));
+        sender.sendMessage("");
+
+        sender.sendMessage(ChatColor.RED + "➤ " + ChatColor.YELLOW + "Social Media:");
+        sender.sendMessage(ColorUtils.translateColorCodes("&7• &bDiscord: &f" + config.getString(ConfigCommandPath.INFO_SOCIAL_DISCORD)));
+        sender.sendMessage(ColorUtils.translateColorCodes("&7• &bWebsite: &f" + config.getString(ConfigCommandPath.INFO_SOCIAL_WEBSITE)));
+        sender.sendMessage(ColorUtils.translateColorCodes("&7• &bStore: &f" + config.getString(ConfigCommandPath.INFO_SOCIAL_STORE)));
+        sender.sendMessage("");
+
+        sender.sendMessage(ChatColor.RED + "➤ " + ChatColor.YELLOW + "Server Information:");
+        sender.sendMessage(ColorUtils.translateColorCodes("&7• &bVersion: &f" + config.getString(ConfigCommandPath.INFO_SERVER_VERSION)));
+        sender.sendMessage(ColorUtils.translateColorCodes("&7• &bGame Mode: &f" + config.getString(ConfigCommandPath.INFO_SERVER_GAMEMODE)));
+        sender.sendMessage("");
+
+        sender.sendMessage(ChatColor.RED + "➤ " + ChatColor.YELLOW + "Staff Contact:");
+        config.getStringList(ConfigCommandPath.INFO_STAFF_CONTACT).forEach(contact ->
+                sender.sendMessage(ColorUtils.translateColorCodes(contact)));
+        sender.sendMessage("");
+
+        sender.sendMessage(ChatColor.RED + "➤ " + ChatColor.YELLOW + "Quick Commands:");
+        config.getStringList(ConfigCommandPath.INFO_QUICK_COMMANDS).forEach(command ->
+                sender.sendMessage(ColorUtils.translateColorCodes(command)));
+        sender.sendMessage("");
+
+        sender.sendMessage(ChatColor.GRAY + "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
+        sender.sendMessage("");
     }
 
     @Override
@@ -68,46 +104,6 @@ public class InformationCommand implements ICommandAPI {
 
         sendServerInformation(sender, Config.getConfig());
         return true;
-    }
-
-    private void sendServerInformation(CommandSender sender, FileConfiguration config) {
-        sender.sendMessage("");
-        sender.sendMessage(ChatColor.GRAY + "━━━━━━━━━━ " + ChatColor.RED + "Server Information" + ChatColor.GRAY + " ━━━━━━━━━━");
-        sender.sendMessage("");
-
-        // Server Rules
-        sender.sendMessage(ChatColor.RED + "➤ " + ChatColor.YELLOW + "Server Rules:");
-        config.getStringList("information.rules").forEach(rule ->
-                sender.sendMessage(ColorUtils.translateColorCodes(rule)));
-        sender.sendMessage("");
-
-        // Social Media
-        sender.sendMessage(ChatColor.RED + "➤ " + ChatColor.YELLOW + "Social Media:");
-        sender.sendMessage(ColorUtils.translateColorCodes("&7• &bDiscord: &f" + config.getString("information.social.discord")));
-        sender.sendMessage(ColorUtils.translateColorCodes("&7• &bWebsite: &f" + config.getString("information.social.website")));
-        sender.sendMessage(ColorUtils.translateColorCodes("&7• &bStore: &f" + config.getString("information.social.store")));
-        sender.sendMessage("");
-
-        // Server Info
-        sender.sendMessage(ChatColor.RED + "➤ " + ChatColor.YELLOW + "Server Information:");
-        sender.sendMessage(ColorUtils.translateColorCodes("&7• &bVersion: &f" + config.getString("information.server.version")));
-        sender.sendMessage(ColorUtils.translateColorCodes("&7• &bGame Mode: &f" + config.getString("information.server.gamemode")));
-        sender.sendMessage("");
-
-        // Staff Contact
-        sender.sendMessage(ChatColor.RED + "➤ " + ChatColor.YELLOW + "Staff Contact:");
-        config.getStringList("information.staffContact").forEach(contact ->
-                sender.sendMessage(ColorUtils.translateColorCodes(contact)));
-        sender.sendMessage("");
-
-        // Quick Commands
-        sender.sendMessage(ChatColor.RED + "➤ " + ChatColor.YELLOW + "Quick Commands:");
-        config.getStringList("information.quickCommands").forEach(command ->
-                sender.sendMessage(ColorUtils.translateColorCodes(command)));
-        sender.sendMessage("");
-
-        sender.sendMessage(ChatColor.GRAY + "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
-        sender.sendMessage("");
     }
 
     @Override
@@ -133,9 +129,8 @@ public class InformationCommand implements ICommandAPI {
     @Override
     public void sendNoPermissionMessage(CommandSender sender) {
         sender.sendMessage(ColorUtils.translateColorCodes(
-                Config.getConfig().getString("information.noPermission")));
+                Config.getConfig().getString(ConfigCommandPath.INFO_NO_PERMISSION)));
     }
-
     @Override
     public boolean isPlayerOnly() {
         return false;
@@ -144,6 +139,6 @@ public class InformationCommand implements ICommandAPI {
     @Override
     public void sendPlayerOnlyMessage(CommandSender sender) {
         sender.sendMessage(ColorUtils.translateColorCodes(
-                Config.getConfig().getString("information.playerOnly")));
+                Config.getConfig().getString(ConfigCommandPath.INFO_PLAYER_ONLY)));
     }
 }
